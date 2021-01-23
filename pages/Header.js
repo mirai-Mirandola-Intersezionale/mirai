18862206527aaf7e7d11932f4553e3d49ed30279
Francesco Luppi <francescoluppi@aol.com>
2021-01-23 22:10:47 +0100

    Adding pages

--- pages/Header.js
+++ pages/Header.js
@@ -5,11 +5,10 @@ export default function Header() {
     return (
         <header className="w-full space-x-4 flex flex-row py-4 px-6 justify-between text-white bg-opacity-0 fixed items-center">
             <div className="logo">
-                <b className="text-2xl">mirai</b>
+                <Link href="/" alt="Home"><b className="text-2xl">mirai</b></Link>
             </div>
             <nav className="">
                 <ul className="navBar space-x-4 flex flex-row ">
-                    <li><Link href="/">Home</Link></li>
                     <li><Link href="/trova">Trova</Link></li>
                     <li><Link href="/contatti">Contatti</Link></li>
                 </ul>
