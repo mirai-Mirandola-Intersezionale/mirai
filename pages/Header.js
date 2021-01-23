044c0b4f2865f0705315b55835ed9ff73a5a391f
Francesco Luppi <francescoluppi@aol.com>
2021-01-23 19:27:36 +0100

    Update style

--- pages/Header.js
+++ pages/Header.js
@@ -3,17 +3,17 @@ import Link from 'next/link'
 
 export default function Header() {
     return (
-        <header className="flex flex-row">
+        <header className="space-x-4 flex flex-row p-4">
             <div className="logo">
                 <b>mirai</b>
             </div>
-            <div className="navBar">
-                <ul>
+            <nav className="">
+                <ul className="navBar space-x-4 flex flex-row">
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/trova">Trova</Link></li>
                     <li><Link href="/contatti">Contatti</Link></li>
                 </ul>
-            </div>
+            </nav>
             
 
 
