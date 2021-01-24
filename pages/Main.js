d58814b001b1537869ee7b1059784251872c777c
Francesco Luppi <francescoluppi@aol.com>
2021-01-24 13:11:28 +0100

    Adding new red palette

--- pages/Main.js
+++ pages/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Main(){
     return (
-        <main className="bg-red-400 h-screen w-full">
+        <main className="bg-red-500 h-screen w-full">
             
         </main>
     )
