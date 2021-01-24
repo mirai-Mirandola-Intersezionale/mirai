534d17d498b71d7280d830b74a2ae3a6f7157c8c
Francesco Luppi <francescoluppi@aol.com>
2021-01-24 14:41:38 +0100

    Adding new links in head and manifest and pwa

--- pages/Header.js
+++ pages/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 
 //Components
-import SimpleToggle from './SimpleToggle'
+import SimpleToggle from '../components/SimpleToggle'
 
 export default function Header() {
     return (
