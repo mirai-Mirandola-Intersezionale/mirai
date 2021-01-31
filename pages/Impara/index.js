e876766cc3ca984da57abec3c8d7e5fd68c80a7f
Francesco Luppi <francescoluppi@aol.com>
2021-01-31 22:13:53 +0100

    Updating style

--- pages/Impara/index.js
+++ pages/Impara/index.js
@@ -5,15 +5,15 @@ const BlogIndex = ({ allPostsData }) => {
   return (
     <div className="max-w-screen-md mx-auto grid grid-flow-col gap-4 mt-10">
           {allPostsData.map(({ slug, date, title, excerpt }) => (
-            <div key={slug} className="rounded-md shadow-sm p-4">
-                  <Link key={slug} href="/Impara/[slug]" as={`/Impara/${slug}`} className="cursor-pointer">
-                      <h1>
-                        {title}
-                      </h1>
-                  </Link>
-                  <p>{excerpt}</p>
-                  <small>{date}</small>
-            </div>
+            <Link key={slug} href="/Impara/[slug]" as={`/Impara/${slug}`}>
+              <div key={slug} className="rounded-md shadow-sm p-4 cursor-pointer">
+                    <h2 className="text-2xl">
+                      {title}
+                    </h2>
+                    <p className="text-base mt-2 text-black text-opacity-50">{excerpt}</p>
+                    <p className="text-xs mt-1">{date}</p>
+              </div>
+            </Link>
           ))}
     </div>
   );
