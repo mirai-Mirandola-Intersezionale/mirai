5138b3ae304fdebd012d72fbe39ed17aa9918331
Francesco Luppi <francescoluppi@aol.com>
2021-01-31 22:24:47 +0100

    fix: style

--- pages/Impara/index.js
+++ pages/Impara/index.js
@@ -3,7 +3,7 @@ import { getSortedPosts } from '../../lib/posts';
 
 const BlogIndex = ({ allPostsData }) => {
   return (
-    <div className="max-w-screen-md mx-auto grid grid-flow-col gap-4 mt-10">
+    <div className="max-w-screen-md mx-auto grid grid-flow-row gap-4 mt-10 px-4">
           {allPostsData.map(({ slug, date, title, excerpt }) => (
             <Link key={slug} href="/Impara/[slug]" as={`/Impara/${slug}`}>
               <div key={slug} className="rounded-md shadow-sm p-4 cursor-pointer">
