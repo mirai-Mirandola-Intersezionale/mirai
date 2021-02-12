ba6b165f7113633cc6225f53785cf279336aef9c
Francesco Luppi <francescoluppi@aol.com>
2021-02-12 20:49:19 +0100

    ADD: copyright

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Footer(){
     return (
         <>
-            <footer className=""></footer>
+            <footer className="">
+                <small className="copyright">&copy; Copyright 2021 - {new Date().currentYear()}, Example Corporation</small>
+            </footer>
         </>
     )
 }
