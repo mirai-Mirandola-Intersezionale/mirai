68b1740209e5b2edf9d5f0e651c8606f1a3eb401
Francesco Luppi <francescoluppi@aol.com>
2021-02-13 09:50:30 +0100

    ADD: style changes to footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,13 +3,8 @@ import React from 'react'
 export default function Footer(){
     return (
         <>
-<<<<<<< HEAD
             <footer className="bg-gray-200">
-                <small className="copyright text-center">&copy; Copyright 2021 - {new Date().currentYear()}, Example Corporation</small>
-=======
-            <footer className="">
-                <small className="copyright">&copy; Copyright 2021 - {new Date().getFullYear()}, Example Corporation</small>
->>>>>>> b993c56a3e7da45203d5f7d880d33e96f47da745
+                <small className="copyright text-center">&copy; Copyright 2021 - {new Date().getFullYear()}, Example Corporation</small>
             </footer>
         </>
     )
