d8dfaf0e0c817856b3ed68c8f32f8cca1729b084
Francesco Luppi <francescoluppi@aol.com>
2021-02-15 18:41:15 +0100

    ADD: reindirizzamento per i link discord e github

--- next.config.js
+++ next.config.js
@@ -3,7 +3,21 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
     pwa: {
         dest: 'public'
-    }
+    },
+    async redirects() {
+    return [
+      {
+        source: '/discord',
+        destination: 'https://discord.gg/dRmhEgGsWH',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/mirai-plus/mirai',
+        permanent: true,
+      },
+    ]
+  },
 })
 
 const withMDX = require('@next/mdx')({
