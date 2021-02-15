09f893f79e6ecc36364cbfae2da1349b6ab507e5
Francesco Luppi <francescoluppi@aol.com>
2021-02-15 18:42:44 +0100

    FIX: spostato collegamento redirect

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ module.exports = withPWA({
     pwa: {
         dest: 'public'
     },
-    async redirects() {
-    return [
-      {
-        source: '/discord',
-        destination: 'https://discord.gg/dRmhEgGsWH',
-        permanent: true,
-      },
-      {
-        source: '/github',
-        destination: 'https://github.com/mirai-plus/mirai',
-        permanent: true,
-      },
-    ]
-  },
 })
 
 const withMDX = require('@next/mdx')({
@@ -26,4 +12,21 @@ const withMDX = require('@next/mdx')({
   module.exports = withMDX({
     pageExtensions: ['js', 'jsx', 'mdx'],
   })
+
+  module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/discord',
+          destination: 'https://discord.gg/dRmhEgGsWH',
+          permanent: true,
+        },
+        {
+          source: '/github',
+          destination: 'https://github.com/mirai-plus/mirai',
+          permanent: true,
+        },
+      ]
+    },
+  }
   
