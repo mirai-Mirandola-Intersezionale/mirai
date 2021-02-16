f3ee34b0150675bf8cd50afe34c2b0484e648776
Francesco Luppi <francescoluppi@aol.com>
2021-02-16 19:52:23 +0100

    FIX: rimosso il rewrite

--- next.config.js
+++ next.config.js
@@ -29,12 +29,7 @@ const withMDX = require('@next/mdx')({
       ]
     },
     async rewrites() {
-    return [
-      {
-        source: '/newsletter',
-        destination: 'https://sleepy-morse-a042c4.netlify.app/',
-      },
-    ]
+    return [{}]
   },
   }
   
