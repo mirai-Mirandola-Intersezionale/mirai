4bb2d43292d644bb2fdc5f87d6c98df069cff335
Francesco Luppi <francescoluppi@aol.com>
2021-02-16 12:05:21 +0100

    DEL: linee di codice non utilizzate
    Eliminate righe di codice che si trovavano ancora nel file dalle ultime modifiche

--- pages/Main.js
+++ pages/Main.js
@@ -11,12 +11,6 @@ export default function Main(){
         <>
         <main className="lg:h-screen bg-red-500  text-white  ">
             <div className="px-4 pt-6 pb-24 max-w-screen-xl mx-auto lg:h-full">
-                {/* <Image src="/img/m.svg" alt="M" width={308} height={196}/>
-                <Image src="/img/i_1.svg" alt="I" width={177} height={231}/>
-                <Image src="/img/r.svg" alt="R" width={198} height={257}/>
-                <Image src="/img/a.svg" alt="A" width={206} height={196}/>
-                <Image src="/img/i_2.svg" alt="I" width={177} height={270}/> */}
-
                 <h1 className="text-7xl font-extrabold">mirai</h1>
                 <h3 className="text-2xl font-light">Collettivo autonomo intersezionale</h3>
                 <p className="font-normal mt-4">Risorse accessibili per tutt* </p>
