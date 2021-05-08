efba6bb5758353817bcf3759c577259ab503f200
frappaaa <francescoluppi@aol.com>
2021-05-08 22:38:03 +0200

    Remove style

--- components/navBar.js
+++ components/navBar.js
@@ -3,10 +3,7 @@ import SearchBar from "./SearchBar";
 
 export default function NavBar() {
   return (
-    <div
-      className="navbar"
-      style={{ display: "flex", gridGap: "10px", marginBottom: "30px" }}
-    >
+    <div>
       <DetailBar />
       <SearchBar />
     </div>
